892a683bc80e3b4b79a5ef14d08c721876986e5a
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2014-05-11 18:39:12 +0100

    Implement HashedStateBuilder for user ease

---
+++ src/main/java/uk/ac/imperial/state/HashedStateBuilder.java
@@ -0,0 +1,54 @@
+package uk.ac.imperial.state;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Class for building {@link uk.ac.imperial.state.HashedState}
+ * without having to explicitly create the Map
+ */
+public class HashedStateBuilder {
+    private Map<String, Map<String, Integer>> tokenCounts = new HashMap<>();
+
+    /**
+     * Adds the specified token and count to the place
+     * @param place
+     * @param token
+     * @param count
+     * @return builder
+     */
+    public HashedStateBuilder placeWithToken(String place, String token, int count) {
+        Map<String, Integer> tokens = getTokens(place);
+        tokens.put(token, count);
+        return this;
+    }
+
+    /**
+     *
+     * Adds all tokenValues to the place
+     * @param place
+     * @param tokenValues
+     * @return builder
+     */
+    public HashedStateBuilder placeWithTokens(String place, Map<String, Integer> tokenValues) {
+        Map<String, Integer> tokens = getTokens(place);
+        tokens.putAll(tokenValues);
+        return this;
+    }
+
+    /**
+     *
+     * @param place place id in state
+     * @return corresponding map for the place
+     */
+    private Map<String, Integer> getTokens(String place) {
+        if (!tokenCounts.containsKey(place)) {
+            tokenCounts.put(place, new HashMap<String, Integer>());
+        }
+        return tokenCounts.get(place);
+    }
+
+    public HashedState build() {
+        return new HashedState(tokenCounts);
+    }
+}
---
+++ src/test/java/uk/ac/imperial/state/HashedStateBuilderTest.java
@@ -0,0 +1,87 @@
+package uk.ac.imperial.state;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class HashedStateBuilderTest {
+
+    HashedStateBuilder builder;
+
+    @Before
+    public void setUp() {
+        builder = new HashedStateBuilder();
+    }
+
+    @Test
+    public void simple() {
+        builder.placeWithToken("P1", "Default", 1);
+        HashedState state = builder.build();
+        Map<String, Integer> map = state.getTokens("P1");
+        int actual = map.get("Default");
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void simpleOverwrites() {
+        builder.placeWithToken("P1", "Default", 1);
+        builder.placeWithToken("P1", "Default", 6);
+        HashedState state = builder.build();
+        Map<String, Integer> map = state.getTokens("P1");
+        int actual = map.get("Default");
+        assertEquals(6, actual);
+    }
+
+    @Test
+    public void multipleColoredTokens() {
+        builder.placeWithToken("P1", "Default", 1);
+        builder.placeWithToken("P1", "Red", 6);
+        HashedState state = builder.build();
+        Map<String, Integer> map = state.getTokens("P1");
+        assertEquals(2, map.size());
+
+        int actualDefault = map.get("Default");
+        assertEquals(1, actualDefault);
+        int actualRed = map.get("Red");
+        assertEquals(6, actualRed);
+    }
+
+    @Test
+    public void multiplePlaces() {
+        builder.placeWithToken("P1", "Default", 1);
+        builder.placeWithToken("P2", "Default", 6);
+        HashedState state = builder.build();
+        Map<String, Integer> mapP1 = state.getTokens("P1");
+        int actualP1 = mapP1.get("Default");
+        assertEquals(1, actualP1);
+        Map<String, Integer> mapP2 = state.getTokens("P2");
+        int actualP2 = mapP2.get("Default");
+        assertEquals(6, actualP2);
+
+    }
+
+    @Test
+    public void addMultipleTokens() {
+        Map<String, Integer> tokens = new HashMap<>();
+        tokens.put("Default", 1);
+        tokens.put("Red", 6);
+        builder.placeWithTokens("P1", tokens);
+        HashedState state = builder.build();
+        Map<String, Integer> map = state.getTokens("P1");
+        assertEquals(2, map.size());
+
+        int actualDefault = map.get("Default");
+        assertEquals(1, actualDefault);
+        int actualRed = map.get("Red");
+        assertEquals(6, actualRed);
+
+    }
+
+
+
+}
